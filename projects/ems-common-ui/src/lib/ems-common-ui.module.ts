06e76806880545668f667118f4f0ae16151c437f
irtahknayan <nayan.khatri@outlook.com>
2022-10-26 21:56:01 +0530

    added header & layout components

--- projects/ems-common-ui/src/lib/ems-common-ui.module.ts
+++ projects/ems-common-ui/src/lib/ems-common-ui.module.ts
@@ -1,11 +1,15 @@
 import { NgModule } from '@angular/core';
 import { EmsCommonUiComponent } from './ems-common-ui.component';
+import { HeaderComponent } from './header/header.component';
+import { LayoutComponent } from './layout/layout.component';
 
 
 
 @NgModule({
   declarations: [
-    EmsCommonUiComponent
+    EmsCommonUiComponent,
+    HeaderComponent,
+    LayoutComponent
   ],
   imports: [
   ],
